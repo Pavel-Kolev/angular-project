9f44d1c09856b322f36ebd093d425fca4a588509
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2024-03-24 16:40:17 +0200

    html + css
    basic html structures and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SectionComponent } from './section/section.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SectionComponent,
+    FooterComponent,
+    HeroComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
